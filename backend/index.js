77019a9573b1f226d49d77f2f603967d46625508
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-03 11:50:44 +0530

    port, bug fixes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 // writing basic express boilerplate code,
 //with express.json() middleware
-import {createTodo, updateTodo} from "./types.js";
+const {createTodo, updateTodo} = require( "./types.js");
 const express = require("express");
 const app = express();
 
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-app.post("/toto", function(req, res){
+app.post("/toto", async function(req, res){
     const createPayLoad = req.body;
     const parsedPayLoad = createTodo.safeParse(createPayLoad);
 
@@ -19,13 +19,37 @@ app.post("/toto", function(req, res){
         }
       )
     }
+
+    try {
+      const todo = await todo.create({
+          title: createPayLoad.title,
+          description: createPayLoad.description,
+          completed: false
+      });
+
+      // If todo creation is successful, return a 201 response with a success message
+      res.status(201).json({ 
+          message: "Todo created successfully", 
+          todo: todo 
+      });
+  } catch (error) {
+      // If there's an error during todo creation, return a 500 response with the error details
+      res.status(500).json({ 
+          message: "An error occurred while creating the todo", 
+          error: error.message 
+      });
+  }
 })
 
-app.get("/todos", function(req, res){
+app.get("/todos", async function(req, res){
+    const todos = await todos.find({});
+    res.json({
+      todos
+    })
 
 })
 
-app.put("/completed", function(req, res){
+app.put("/completed", async function(req, res){
     const updatePayLoad = req.body;
     const parsedUPayLoad = updateTodo.safeParse(updatePayLoad);
 
@@ -36,4 +60,15 @@ app.put("/completed", function(req, res){
         }
       )
     }
+
+    await todo.update({
+      _id: req.body.id
+    },{
+      completed: true
+    })
+    res.json({
+      msg: "Marked as completed"
+    })
 })
+
+app.listen(3000);
