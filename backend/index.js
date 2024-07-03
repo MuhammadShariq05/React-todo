9448708146351e4db6c7da90803c211b1c5d17a2
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-03 10:53:43 +0530

    included put, post file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 // writing basic express boilerplate code,
 //with express.json() middleware
-
+import {createTodo, updateTodo} from "./types.js";
 const express = require("express");
 const app = express();
+
 
 // Middleware
 app.use(express.json());
 
 app.post("/toto", function(req, res){
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad);
 
+    if(!parsedPayLoad.success){
+      res.send(411).json(
+        {
+          msg: "Wrong Inputs", 
+        }
+      )
+    }
 })
 
 app.get("/todos", function(req, res){
@@ -16,5 +26,14 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
-  
+    const updatePayLoad = req.body;
+    const parsedUPayLoad = updateTodo.safeParse(updatePayLoad);
+
+    if(!parsedUPayLoad.success){
+      res.send(411).json(
+        {
+          msg: "Wrong inputs",
+        }
+      )
+    }
 })
